14d78fe8c9feb134a9d34a12fa6b0c6c32503452
mohammad-dehghanifard <m.deheghanifard@gmail.com>
2023-06-04 05:53:09 -0700

    create otp and register screen ui class

---
+++ lib/screens/get_opt_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GetOtpScreen extends StatelessWidget {
+  const GetOtpScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
