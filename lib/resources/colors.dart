d018036aafb198eb4a6786bf16f0e9b5932c0de6
mohammad-dehghanifard <m.deheghanifard@gmail.com>
2023-06-10 08:06:48 -0700

    impl SolidLightColors class

--- lib/resources/colors.dart
+++ lib/resources/colors.dart
@@ -1,4 +1,11 @@
+import 'package:flutter/material.dart';
+
 class SolidLightColors{
   SolidLightColors._();
-
+  static const Color title = Colors.black;
+  static const Color hint = Colors.grey;
+  static const Color appBar = Colors.white;
+  static const Color borderColor = Colors.grey;
+  static const Color focusedBorderColor = Colors.blue;
+  static const Color primary = Colors.blue;
 }
