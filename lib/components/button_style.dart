60c2260adeff6dcfb7731b35e53993c248fe85a9
mohammad-dehghanifard <m.deheghanifard@gmail.com>
2023-06-12 04:01:25 -0700

    impl mainButtonStyle

--- lib/components/button_style.dart
+++ lib/components/button_style.dart
@@ -1,5 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_watch_store/resources/colors.dart';
+import 'package:flutter_watch_store/resources/dimens.dart';
+
 class WatchButtonStyle{
   WatchButtonStyle._();
 
-
+  static ButtonStyle mainButtonStyle = ButtonStyle(
+    shape: MaterialStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(Dimens.medium))),
+    backgroundColor: const MaterialStatePropertyAll(SolidLightColors.primary)
+  );
 }
