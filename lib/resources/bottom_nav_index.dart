79b46efe066c336280dd9dbb8e61a7ce20a4a8f4
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-07-22 01:47:45 -0700

    impl BottomNavIndex

---
+++ lib/resources/bottom_nav_index.dart
@@ -0,0 +1,5 @@
+class BottomNavIndex{
+  static int homeIndex = 0;
+  static int basketIndex = 0;
+  static int profileIndex = 0;
+}
--- lib/screens/main_screen/screens/main_screen.dart
+++ lib/screens/main_screen/screens/main_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_watch_store/screens/main_screen/screens/basket_screen.dart';
+import 'package:flutter_watch_store/screens/main_screen/screens/home_screen.dart';
+import 'package:flutter_watch_store/screens/main_screen/screens/profile_screen.dart';
 import 'package:flutter_watch_store/screens/main_screen/widgets/watch_bottom_navigation.dart';
 
 class MainScreen extends StatelessWidget {
@@ -10,19 +13,39 @@ class MainScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          // screen
-          Positioned(
-            top: 0,
-            right: 0,
-            left: 0,
-            bottom: size.height * 0.1,
-            child: Container(
-              width: size.width,
-              height: size.height,
-              color: Colors.lightGreen,
-            ),
-          ),
+          IndexedStack(
+            index: 0,
+            children: [
+              // home screen
+              Positioned(
+                top: 0,
+                right: 0,
+                left: 0,
+                bottom: size.height * 0.1,
+                child: const HomeScreen(),
+              ),
 
+              // basket screen
+              Positioned(
+                top: 0,
+                right: 0,
+                left: 0,
+                bottom: size.height * 0.1,
+                child: const BasketScreen(),
+              ),
+
+              // profile screen
+              Positioned(
+                top: 0,
+                right: 0,
+                left: 0,
+                bottom: size.height * 0.1,
+                child: const ProfileScreen(),
+              ),
+
+
+            ],
+          ),
           // bottom navigation
           const Positioned(
             bottom: 0,
