f07f5521df73365be50fe184afc529b7f106d881
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-08-17 01:00:54 -0700

    refactor home slider and convert to custom widget

--- lib/screens/main_screen/screens/home_screen.dart
+++ lib/screens/main_screen/screens/home_screen.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_watch_store/resources/colors.dart';
-import 'package:flutter_watch_store/resources/dimens.dart';
-import 'package:flutter_watch_store/resources/texts.dart';
-import '../../../components/text_style.dart';
-import '../../../gen/assets.gen.dart';
+import '../../../widgets/home_search_bar.dart';
 import '../../../widgets/home_slider.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -18,30 +13,8 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             //search bar
-            Container(
-              margin: EdgeInsets.all(Dimens.medium),
-              width: size.width,
-              height: size.height / 18,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(64),
-                color: SolidLightColors.searchBar,
-                boxShadow: const [
-                  BoxShadow(
-                    blurRadius: 5,
-                    offset: Offset(0,3),
-                    color: SolidLightColors.searchShadow
-                  )
-                ],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  SvgPicture.asset(Assets.svg.search),
-                  Text(WatchTexts.searchHintTxt,style: LightTextStyle.formHintStyle),
-                  Image.asset(Assets.png.mainLogo.path,scale: 4)
-                ],
-              ),
-            ),
+            HomeSearchBar(size: size),
+            // slider
             HomeSlider()
           ],
         ),
@@ -49,6 +22,8 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+
 
 
 
