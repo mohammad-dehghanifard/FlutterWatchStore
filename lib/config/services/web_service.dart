21175317b3a0dcd2c6036a474c77d32ed2ee998a
mohammad <m.deheghanifard@gmail.com>
2024-04-06 16:01:44 +0330

    send token in request headers

--- lib/config/services/web_service.dart
+++ lib/config/services/web_service.dart
@@ -1,10 +1,17 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_watch_store/config/services/service_locator.dart';
+import 'package:flutter_watch_store/core/resources/storage_key.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class WebService {
   final Dio dio = Dio(
     BaseOptions(
       baseUrl: "https://watchstore.sasansafari.com/public/api/v1",
       validateStatus: (status) => status! < 500,
+      headers: {
+        if(di<SharedPreferences>().getString(StorageKey.token) != null)
+        "Authorization" : "Bearer ${di<SharedPreferences>().getString(StorageKey.token)}"
+      }
     )
   );
 
