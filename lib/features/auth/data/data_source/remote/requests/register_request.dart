5d8b0eefd6687d4a3038a6c4bfb0daf487923719
mohammad <m.deheghanifard@gmail.com>
2024-04-07 12:04:31 +0330

    impl register usecase

--- lib/features/auth/data/data_source/remote/requests/register_request.dart
+++ lib/features/auth/data/data_source/remote/requests/register_request.dart
@@ -1,4 +1,7 @@
 class RegisterRequest {
+  RegisterRequest({
+    this.phone,this.name,this.postalCod,this.address,this.lng,this.lat
+});
   int? phone,postalCod;
   String? name,address;
   double? lat,lng;
--- lib/features/auth/domain/usecases/register_usecase.dart
+++ lib/features/auth/domain/usecases/register_usecase.dart
@@ -1,2 +1,11 @@
 import 'package:flutter_watch_store/core/usecase/use_case.dart';
+import 'package:flutter_watch_store/features/auth/data/data_source/remote/requests/register_request.dart';
+import 'package:flutter_watch_store/features/auth/domain/repository/auth_repository.dart';
 
+class RegisterUseCase extends UseCase<Map<String,dynamic>,RegisterRequest> {
+  RegisterUseCase(this.repository);
+  final AuthRepository repository;
+  @override
+  Future<Map<String, dynamic>> call(RegisterRequest params) async => repository.registerUser(params);
+
+}
