be34db026aa4d18b22b872289849f405c7a3c21d
mohammad <m.deheghanifard@gmail.com>
2024-04-09 12:24:18 +0330

    impl HomeRepositoryImpl class

--- lib/features/home/data/repository/home_repository_impl.dart
+++ lib/features/home/data/repository/home_repository_impl.dart
@@ -2,3 +2,10 @@ import 'package:flutter_watch_store/features/home/data/data_source/remote/home_a
 import 'package:flutter_watch_store/features/home/domain/entities/home_entity.dart';
 import 'package:flutter_watch_store/features/home/domain/repository/home_repository.dart';
 
+class HomeRepositoryImpl extends IHomeRepository {
+  HomeRepositoryImpl(this.apiProvider);
+  final HomeApiProvider apiProvider;
+  @override
+  Future<HomeEntity?> fetchHomeData() => apiProvider.fetchHomeData();
+
+}
