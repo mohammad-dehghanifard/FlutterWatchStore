2e9e0f4ec28d1a7656e731e61178c6919269acba
mohammad <m.deheghanifard@gmail.com>
2024-04-13 08:54:01 +0330

    impl ProductDetailApiProvider class

--- lib/features/home/data/repository/home_repository_impl.dart
+++ lib/features/home/data/repository/home_repository_impl.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_watch_store/features/home/data/data_source/remote/home_api_provider.dart';
+import 'package:flutter_watch_store/features/home/data/models/home_model.dart';
 import 'package:flutter_watch_store/features/home/domain/entities/home_entity.dart';
 import 'package:flutter_watch_store/features/home/domain/repository/home_repository.dart';
 
@@ -6,6 +8,13 @@ class HomeRepositoryImpl extends IHomeRepository {
   HomeRepositoryImpl(this.apiProvider);
   final HomeApiProvider apiProvider;
   @override
-  Future<HomeEntity?> fetchHomeData() => apiProvider.fetchHomeData();
+  Future<HomeEntity?> fetchHomeData() async {
+    final Response response = await apiProvider.fetchHomeData();
+    if(response.statusCode == 200) {
+      final HomeEntity entity = HomeModel.fromJson(response.data);
+      return entity;
+    }
+    return null;
+  }
 
 }
--- lib/features/product/data/data_source/remote/product_detail_api_provider.dart
+++ lib/features/product/data/data_source/remote/product_detail_api_provider.dart
@@ -2,11 +2,11 @@ import 'package:dio/dio.dart';
 import 'package:flutter_watch_store/config/services/service_locator.dart';
 import 'package:flutter_watch_store/config/services/web_service.dart';
 
-class HomeApiProvider {
+class ProductDetailApiProvider{
   final WebService _webService = di<WebService>();
-  
-  Future<Response> fetchHomeData() async {
-    final Response response = await _webService.getRequest(url: "/home");
+
+  Future<Response> fetchDetail(int id) async {
+    final Response response = await _webService.getRequest(url: "/product_details/$id");
     return response;
   }
 }
